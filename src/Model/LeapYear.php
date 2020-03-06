299edaacf946762afb206fd1fe9401174212d7f7
Pierre Louis Martin <pl.martin.mess@gmail.com>
2020-03-06 14:06:24 +0100

    Implémentation de la logique métier

--- src/Model/LeapYear.php
+++ src/Model/LeapYear.php
@@ -9,7 +9,21 @@ class LeapYear
 
     public static function isLeapYear(int $year): bool
     {
-        return true;
+        if ($year % 400 === 0) {
+            return true;
+        }
+
+        if (($year % 100 === 0) && ($year % 400 !== 0)) {
+            return false;
+        }
+
+        if (($year % 4 === 0) && ($year % 100 !== 0)) {
+            return true;
+        }
+
+        if ($year % 4 !== 0) {
+            return false;
+        }
 
 
     }
